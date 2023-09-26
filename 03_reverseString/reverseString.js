dddf2ad384bc9b198dfd32897e4f034a750c55d9
Carmen Morata <cmoratagarcia@gmail.com>
2023-09-26 14:56:16 -0700

    Create function to pass all tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  let strArray = string.split("");
+  let resultArray = [];
+  for (let i = 0; i < strArray.length; i++) {
+    resultArray.unshift(strArray[i]);
+  }
+  return resultArray.join("");
 };
 
 // Do not edit below this line
