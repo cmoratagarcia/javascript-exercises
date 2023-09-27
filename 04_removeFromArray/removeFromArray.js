0741889cd076760080d33982ffbaf933da9a25fc
Carmen Morata <cmoratagarcia@gmail.com>
2023-09-26 17:05:13 -0700

    Create function to pass all tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...values) {
+  let resultArray = [];
+  for (item of array) {
+    if (!values.includes(item)) {
+      resultArray.push(item);
+    }
+  }
+  return resultArray;
 };
-
+console.log(removeFromArray([1, "mom", 2, 3], "mom", 2));
 // Do not edit below this line
 module.exports = removeFromArray;
