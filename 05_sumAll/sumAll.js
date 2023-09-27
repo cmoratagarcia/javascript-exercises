7a139acbfe9659fd2991cf348dc5c240fc8747c4
Carmen Morata <cmoratagarcia@gmail.com>
2023-09-26 17:21:17 -0700

    Create function to pass all tests

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
-
+const sumAll = function (a, b) {
+  let sum = 0;
+  if (typeof a !== "number" || typeof b !== "number" || a < 0 || b < 0) {
+    return "ERROR";
+  }
+  if (a < b) {
+    while (a <= b) {
+      sum += a;
+      a++;
+    }
+  } else {
+    while (b <= a) {
+      sum += b;
+      b++;
+    }
+  }
+  return sum;
 };
 
 // Do not edit below this line
