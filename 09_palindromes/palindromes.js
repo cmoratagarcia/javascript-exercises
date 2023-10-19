780c4414ca4552b0f32f6b1d3b2c8660da3173da
Carmen Morata <cmoratagarcia@gmail.com>
2023-10-19 16:50:57 -0700

    Create function to pass test requirements

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (str) {
+  //remove punctuation and spaces
+  let cleanStr = str
+    .toLowerCase()
+    .replace(/[^\w\s\']|_/g, "")
+    .replace(/\s+/g, "");
 
+  let compareStr = cleanStr.split("").reverse().join("");
+
+  return cleanStr === compareStr;
 };
 
 // Do not edit below this line
