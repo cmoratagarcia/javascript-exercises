c4eeab56cefb789f57b2a08777fbf50cbc028723
Carmen Morata <cmoratagarcia@gmail.com>
2023-10-19 11:06:32 -0700

    Create functions to pass all tests

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,27 +1,40 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  return array.reduce((total, current) => total * current, 1);
 };
 
-const power = function() {
-	
+const power = function (number, power) {
+  let result = number;
+  for (let i = 1; i < power; i++) {
+    result = result * number;
+  }
+  return result;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  let result = number;
+  let multiplier = number - 1;
+  if (number <= 1) {
+    result = 1;
+  } else {
+    for (let i = 1; i < number; i++) {
+      result = result * multiplier; //6
+      multiplier--; //1
+    }
+  }
+  return result;
 };
-
 // Do not edit below this line
 module.exports = {
   add,
@@ -29,5 +42,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
