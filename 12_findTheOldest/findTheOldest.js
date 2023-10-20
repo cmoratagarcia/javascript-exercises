149ee52bef5d370e6225d179cc4866a7220bc6fc
Carmen Morata <cmoratagarcia@gmail.com>
2023-10-20 11:24:14 -0700

    Create function to pass all tests

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,15 @@
-const findTheOldest = function() {
+const findTheOldest = function (array) {
+  let currentYear = new Date().getFullYear();
+  for (let person of array) {
+    if (!person.yearOfDeath) {
+      person.yearOfDeath = currentYear;
+    }
+  }
+  let agesArray = array.sort((a, b) =>
+    a.yearOfDeath - a.yearOfBirth > b.yearOfDeath - b.yearOfBirth ? -1 : 1
+  );
 
+  return agesArray[0];
 };
 
 // Do not edit below this line
