a2ba17102c83c8d13adf97e15788e5790ce95da4
Carmen Morata <cmoratagarcia@gmail.com>
2023-10-19 17:50:57 -0700

    Create function to pass all tests

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function (n) {
+  let item = Number(n);
+  let firstNum = 1;
+  let secondNum = 0;
+  let containerArr = [];
 
+  if (item < 0) {
+    return "OOPS";
+  }
+
+  for (let i = 0; i < item; i++) {
+    firstNum = firstNum + secondNum;
+    containerArr.push(firstNum);
+    secondNum = firstNum + secondNum;
+    containerArr.push(secondNum);
+  }
+  return containerArr[item - 1];
 };
 
 // Do not edit below this line
